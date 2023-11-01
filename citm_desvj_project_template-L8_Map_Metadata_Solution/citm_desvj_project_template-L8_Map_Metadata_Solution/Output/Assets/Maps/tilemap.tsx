bd026bb5459d8b7a73ad0f589dff4c961d5c8b7b
YunqianAo <79161526+YunqianAo@users.noreply.github.com>
2023-11-01 23:10:48 +0100

    preparar el mapa y el tilemapa

---
+++ citm_desvj_project_template-L8_Map_Metadata_Solution/citm_desvj_project_template-L8_Map_Metadata_Solution/Output/Assets/Maps/tilemap.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tilemap" tilewidth="18" tileheight="18" spacing="1" tilecount="112" columns="16">
+ <image source="../../../../../../Downloads/kenney_pixel-platformer-food-expansion/Tilemap/tilemap.png" width="303" height="132"/>
+ <wangsets>
+  <wangset name="未命名集" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+  </wangset>
+ </wangsets>
+</tileset>
