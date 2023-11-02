15cce291b4c4420389a5ba4a2c05c357de1bf477
YunqianAo <79161526+YunqianAo@users.noreply.github.com>
2023-11-02 23:54:02 +0100

    map edit

--- citm_desvj_project_template-L8_Map_Metadata_Solution/citm_desvj_project_template-L8_Map_Metadata_Solution/Output/Assets/Maps/tilemap.tsx
+++ citm_desvj_project_template-L8_Map_Metadata_Solution/citm_desvj_project_template-L8_Map_Metadata_Solution/Output/Assets/Maps/tilemap.tsx
@@ -1,9 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="tilemap" tilewidth="18" tileheight="18" spacing="1" tilecount="112" columns="16">
- <image source="../../../../../../Downloads/kenney_pixel-platformer-food-expansion/Tilemap/tilemap.png" width="303" height="132"/>
- <wangsets>
-  <wangset name="未命名集" type="mixed" tile="-1">
-   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
-  </wangset>
- </wangsets>
+<tileset version="1.9" tiledversion="1.9.2" name="tilemap" tilewidth="17" tileheight="17" spacing="1" tilecount="112" columns="16">
+ <image source="kenney_pixel-platformer-food-expansion/Tilemap/tilemap.png" width="303" height="132"/>
 </tileset>
